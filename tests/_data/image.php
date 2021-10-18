a01581f514e95d82c9f663ee4dff853e938d1fd1
NikitaZhidov <nzhidov02@gmail.com>
2021-10-18 23:52:53 +0300

    Edit README.md

--- tests/_data/image.php
+++ tests/_data/image.php
@@ -3,6 +3,6 @@
         'image1' => [
             'id' => 1,
             'name' => 'tests/upload/EnGxeQWEas.png',
-            'caption' => 'Angular', // hashed 'root' password
+            'caption' => 'Angular',
         ],
     ];
